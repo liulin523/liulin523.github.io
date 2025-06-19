b4b5aee3b506442498eda863c25839142aad4948
liulin523 <liulin523@users.noreply.github.com>
2025-06-19 21:45:54 +0000

    Deploying to gh-pages from @ liulin523/liulin523.github.io@a9aa6d02c49ac88e5e381e65997945088bc3533b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -167,7 +167,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=5VBaQTIAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
