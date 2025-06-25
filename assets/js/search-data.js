3c6733aadba11e5c521576edc75020897a6e51bb
liulin523 <liulin523@users.noreply.github.com>
2025-06-25 21:49:02 +0000

    Deploying to gh-pages from @ liulin523/liulin523.github.io@26d7b80b918020ef0627935f94dc1bcaae42246e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -255,9 +255,9 @@ ninja.data = [{
           description: "Research Assistant (2015-16) now Associate Professor at Southwest Jiaotong University",
           section: "Projects",handler: () => {
               window.location.href = "/projects/wang_xiaowen/";
-            },},{id: "projects-zhuoxuan-summer-xia",
-          title: 'Zhuoxuan (Summer) Xia',
-          description: "PhD student",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/xia_zhuoxuan/";
             },},{id: "projects-xingyu-carol-xu",
